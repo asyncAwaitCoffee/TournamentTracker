f97d97cbdf36bac1b36b3e98e67519af62ed73b8
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-02 21:27:20 +0400

    data connections impl - p2 - sql

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
-    public class SqlConnector : IDataConnection
+    public class TextConnector : IDataConnection
     {
         //TODO - implement CreatePrize method
         /// <summary>
-        /// Saves a new prize to the database
+        /// 
         /// </summary>
-        /// <param name="model">The prize information</param>
-        /// <returns>The prize information with id</returns>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             model.Id = 1;
--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = [];
+        public static IDataConnection Connection;
 
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database)
+            if (db == DatabaseType.SQL)
             {
                 //TODO - SQL connections
                 SqlConnector sql = new();
-                Connections.Add(sql);
+                Connection = sql;
             }
 
-            if (textFiles)
+            else if (db == DatabaseType.TextFile)
             {
                 //TODO - text file connections
-                TextConnection textConnection = new();
-                Connections.Add(textConnection);
+                TextConnector textConnection = new();
+                Connection = textConnection;
             }
+        }
+
+        public static string CnnString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
         }
     }
 }

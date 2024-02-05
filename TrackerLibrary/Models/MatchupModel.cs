df36d9fa8689dbeb01d0e7922b249ba58621cceb
Andrei Stepanov <steam.oni.am@gmail.com>
2024-02-05 17:11:13 +0400

    matchups, tournament logic

--- TrackerLibrary/Models/MatchupModel.cs
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -12,8 +12,17 @@ namespace TrackerLibrary.Models
         /// The unique identifier for the matchup
         /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// The set of teams that were involved in this match
+        /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        /// <summary>
+        /// The winner of the match
+        /// </summary>
         public TeamModel Winner { get; set; }
+        /// <summary>
+        /// Which round this match is a part of
+        /// </summary>
         public int MatchupRound { get; set; }
     }
 }
